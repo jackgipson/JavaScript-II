94dbe03403ba33e6e2dd92f8f996e2d4eb556785
jackgipson <jackjgipson@gmail.com>
2019-02-01 11:16:44 -0800

    answered ? 2 callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -33,7 +33,13 @@ getLength(items, (length) => {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
